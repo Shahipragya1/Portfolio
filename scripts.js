b3d8c7bab6fd559036b1c1b9c22e846f26d0d488
Shahipragya1 <pragyashahi059@gmail.com>
2025-01-13 23:22:06 +0545

    Added contacts and form submit also imporved UI/UX

--- scripts.js
+++ scripts.js
@@ -1,10 +1,20 @@
-// Example: Smooth scrolling for navigation links
+// scripts.js
+
+// Smooth scrolling for navigation links
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-      e.preventDefault();
-  
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth'
-      });
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
     });
   });
+});
+
+// Toggle mobile navigation menu
+const navToggle = document.querySelector('.nav-toggle');
+const navbar = document.querySelector('.navbar');
+
+navToggle.addEventListener('click', () => {
+  navbar.classList.toggle('navbar-open');
+});
